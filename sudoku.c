70581dd3be9f342e0d3a7a019d2142cdab491528
Karen Li <karensweden@gmail.com>
2025-09-22 20:02:15 +0200

    Added typedef strut for the sudoku

--- sudoku.c
+++ sudoku.c
@@ -1,1 +1,9 @@
 # include <stdio.h>
+
+typedef struct {
+    int puzzle[9][9]; //the current status of the puzzle
+    int solution[9][9]; //the compleate solution of the game
+    int initial[9][9]; //the initial state of the puzzle, to track the fixed cells
+    time_t start_time; //tracks when the game starts
+} SudokuGame;
+
