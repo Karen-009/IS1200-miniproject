1c7b5dd5098284c3c13bbc1ced9ba6147bcb6386
zunjeebayar <zunjee.bayarsaikhan@icloud.com>
2025-10-02 13:57:14 +0200

    test

--- minesweeper.c
+++ minesweeper.c
@@ -1,4 +1,5 @@
 # include <stdio.h>
+#include <time.h>
 
 typedef struct {
     int puzzle[9][9]; //the current status of the puzzle
@@ -6,4 +7,6 @@ typedef struct {
     int initial[9][9]; //the initial state of the puzzle, to track the fixed cells
     time_t start_time; //tracks when the game starts
 } SudokuGame;
+
+// hej får du upp denna 
 
