76dc96a150d09348c1110f95fd29134cdbcba6df
Karen Li <karensweden@gmail.com>
2025-10-04 18:36:36 +0200

    Minesweeper code

---
+++ Minesweeper/system.h
@@ -0,0 +1,48 @@
+# define VGA_Buffer 0x8000000
+
+# define VGA_DMA 0x4000100
+
+# define TIMER_base 0x4000020
+
+# define SWITCH_base 0x4000010
+
+# define HEX_base 0x4000050
+
+# define KEY0_base 0x40000d0
+
+# define KEY1_base 0x40000df
+
+// Colors
+#define black     rgb_to_vga(0, 0, 0)
+#define white     rgb_to_vga(255, 255, 255)
+#define red       rgb_to_vga(255, 0, 0)
+#define green     rgb_to_vga(0, 255, 0)
+#define blue      rgb_to_vga(0, 0, 255)
+#define yellow    rgb_to_vga(255, 255, 0)
+#define cyan      rgb_to_vga(0, 255, 255)
+#define magenta   rgb_to_vga(255, 0, 255)
+#define gray      rgb_to_vga(128, 128, 128)
+#define dark_gray rgb_to_vga(64, 64, 64)
+#define light_blue rgb_to_vga(173, 216, 230)
+
+// Game constants
+#define MAX_SIZE 20
+#define EASY_SIZE 8
+#define MEDIUM_SIZE 16
+#define HARD_SIZE 20
+#define EASY_MINES 10
+#define MEDIUM_MINES 35
+#define HARD_MINES 80
+
+// Switch and Key Assignments
+# define SW_l1 0
+# define SW_l2 1
+# define SW_l3 2
+# define SW_up 3
+# define SW_down 4
+# define SW_right 5
+# define SW_left 6
+# define SW_flag 7
+# define SW_reveal 8
+
+# define KEY_enter 0
