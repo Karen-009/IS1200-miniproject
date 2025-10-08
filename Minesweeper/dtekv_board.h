385fd33aa380f0693982abe2933eb11c8b367a48
Karen Li <karensweden@gmail.com>
2025-10-08 11:46:02 +0200

    New

---
+++ Minesweeper/dtekv_board.h
@@ -0,0 +1,61 @@
+#ifndef DTEKV_BOARD_H
+#define DTEKV_BOARD_H
+
+// Memory Addresses for VGA
+#define VGA_Buffer 0x8000000
+#define VGA_DMA 0x4000100
+#define SWITCH_base 0x4000010
+#define KEY1_base 0x40000d0
+#define TIMER_base 0x4000020
+#define HEX_base 0x4000050
+#define SWITCH_BASE 0x4000010  
+
+// Colors 
+#define black     0x00
+#define white     0xFF
+#define red       0xE0
+#define yellow    0xFC
+#define green     0x1C
+#define blue      0x03
+#define cyan      0x1F
+#define magenta   0xE3
+#define gray      0x92
+#define dark_gray 0x49
+#define light_blue 0x9F
+#define orange    0xFC 
+#define light_gray 0xE4 
+#define light_green 0x7C
+#define light_red 0xF4
+#define light_yellow 0xFE
+#define purple    0xA3
+#define brown     0xB2
+#define pink      0xF3
+#define light_cyan 0xBF
+#define light_magenta 0xF3
+#define dark_blue 0x02
+#define dark_green 0x0C
+#define pastel_pink 0xF5 
+#define pastel_yellow 0xFD
+#define pastel_blue 0xBF
+#define pastel_green 0x7D
+#define pastel_purple 0xB3
+#define pastel_orange 0xF6
+#define light_purple 0xB3
+#define light_pink 0xF3
+#define light_orange 0xF6
+
+// Switch and Key Assignments
+#define SW_l1 1     // easy difficulty
+#define SW_l2 2   // medium difficulty
+#define SW_l3 3   // hard  difficulty
+#define SW_up 4   // move cursor up
+#define SW_down 5   // move cursor down
+#define SW_right 6  // move cursor right
+#define SW_left 7      // move cursor left
+#define SW_ACTION_1 8   // toggle flag mode/ erase cell
+#define SW_ACTION_2 9 // reveal cell/ enter digit mode
+#define KEY_enter 0 // confirm action
+//#define KEY_exit 0  // exit game
+
+
+#endif
