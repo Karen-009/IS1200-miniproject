0dcfb723b0f709aed698411eb0d7135f179fe2bb
Karen <karensweden@gmail.com>
2025-10-09 14:52:31 +0200

    Test

--- sudoku_input_vga.h
+++
@@ -1,8 +0,0 @@
-#ifndef SUDOKU_INPUT_VGA_H
-#define SUDOKU_INPUT_VGA_H
-#include "sudoku.h" 
-
-// Function that reads  DTEKV switches and keys and returns the corresponding InputAction
-InputAction get_input_vga(void);
-
-#endif
--- sudoku_puzzles.h
+++
@@ -1,6 +0,0 @@
-#ifndef SUDOKU_PUZZLES_H
-#define SUDOKU_PUZZLES_H
-
-extern const int solved_grid[9][9];
-
-#endif
